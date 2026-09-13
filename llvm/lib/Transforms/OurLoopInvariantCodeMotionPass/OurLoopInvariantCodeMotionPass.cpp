66ac50ed5c56ff81552e93f03a40fa11a8f928a8
Nikola Lazić <mitalazic@gmail.com>
2026-09-13 14:34:38 +0200

    feat: add loop invariant analysis

--- llvm/lib/Transforms/OurLoopInvariantCodeMotionPass/OurLoopInvariantCodeMotionPass.cpp
+++ llvm/lib/Transforms/OurLoopInvariantCodeMotionPass/OurLoopInvariantCodeMotionPass.cpp
@@ -1,15 +1,17 @@
-#include "llvm/IR/Function.h"
+#include "llvm/Analysis/LoopPass.h"
 #include "llvm/Pass.h"
-#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
 namespace {
-struct OurLoopInvariantCodeMotionPass : public FunctionPass {
+// Prebacio sam sablon sa FunctionPass na LoopPass.
+struct OurLoopInvariantCodeMotionPass : public LoopPass {
   static char ID;
-  OurLoopInvariantCodeMotionPass() : FunctionPass(ID) {}
+  OurLoopInvariantCodeMotionPass() : LoopPass(ID) {}
 
-  bool runOnFunction(Function &F) override { return false; }
+  bool runOnLoop(Loop *CurrentLoop, LPPassManager &LoopPassManager) override {
+    return false;
+  }
 };
 } // namespace
 
